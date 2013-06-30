dcecdf9b89b0d202da558f9843f86bb7c698ca7c
Thiago Fernandes Massa <thiagown@gmail.com>
2013-06-30 17:57:47 -0300

    More tests

---
+++ spec/models/credit_card_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe CreditCard do
+  context 'transfer' do
+    it "should send funds to somebody" do
+      pending
+    end
+
+    it "should receive fund from somewhere" do
+      pending
+    end
+
+    it "shouldn't be possible to send more funds than it's avaialable balance" do
+    end
+  end
+end
