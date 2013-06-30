783475ed09c75361400c7b5cc97100f984def027
Thiago Fernandes Massa <thiagown@gmail.com>
2013-06-30 12:30:59 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBank::Application.config.secret_token = '0d07327e6e227dfa49f0aaf997ba2e35ea5a0a6b210170b73fdd84d8587301234f933c3751e37e18ebf38ccd2762e6c1767543e64cf4007a4f7d166d58025865'
